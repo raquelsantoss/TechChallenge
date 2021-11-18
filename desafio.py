c97dbee1e4a39711f18a474ae7d7dbcf6d721833
Raquel Santos <80298086+raquelsantoss@users.noreply.github.com>
2021-11-18 11:24:06 -0300

    Add files via upload

---
+++ desafio.py
@@ -0,0 +1,20 @@
+#Raquel dos Santos Rezende
+expression = str(input('Digite a expressão que será analisada:'))
+countParenthesesOpen = 0
+countParenthesesClose = 0
+
+for letter in range(0, len(expression)):
+    if(expression[letter] == '('):
+        countParenthesesOpen += 1
+    elif(expression[letter] == ')'):
+        countParenthesesClose += 1
+
+    if(countParenthesesClose > countParenthesesOpen):   #validação para parênteses de sentido opostos ")("
+        break 
+
+
+if(countParenthesesOpen != countParenthesesClose):
+    print('Incorrect')
+else:
+    print('Correct')
+
